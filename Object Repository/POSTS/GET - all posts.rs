ba90596a8ded194d1b18749e3761440e5942d06f
Management Trainee Technology Head Office (Chandra) <095047@bfi.co.id>
2022-07-06 15:40:05 +0700

    add

---
+++ Object Repository/POSTS/GET - all posts.rs
@@ -0,0 +1,61 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>GET - all posts</name>
+   <tag></tag>
+   <elementGuidId>7483ad5a-3103-4b00-8379-c609880653bd</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <katalonVersion>8.3.5</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://jsonplaceholder.typicode.com/posts</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+
+
+WS.verifyElementPropertyValue(response, '[0].userId', 1)
+WS.verifyElementPropertyValue(response, '[0].id', 1)
+WS.verifyElementPropertyValue(response, '[0].title', 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit')
+WS.verifyElementPropertyValue(response, '[0].body', 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto')
+
+
+WS.verifyElementPropertyValue(response, '[1].userId', 1)
+WS.verifyElementPropertyValue(response, '[1].id', 2)
+WS.verifyElementPropertyValue(response, '[1].title', 'qui est esse')
+WS.verifyElementPropertyValue(response, '[1].body', 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla')
+
+
+WS.verifyElementPropertyValue(response, '[2].userId', 1)
+WS.verifyElementPropertyValue(response, '[2].id', 3)
+WS.verifyElementPropertyValue(response, '[2].title', 'ea molestias quasi exercitationem repellat qui ipsa sit aut')
+WS.verifyElementPropertyValue(response, '[2].body', 'et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut')</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
---
+++ README.md
@@ -0,0 +1,35 @@
+Testing JsonPlaceholder ApiEndpoint(posts) 
+
+1.GET `https://jsonplaceholder.typicode.com/posts/1` <br>
+digunakan untuk mendapatkan data posts dengan id 1 <br>
+
+2.POST `https://jsonplaceholder.typicode.com/posts` <br>
+digunakan untuk create data posts <br>
+
+`
+{
+    id: 101,
+    title: 'jackie',
+    body: 'chan',
+    userId: 1,
+}
+`
+
+<br>
+
+3.PUT `https://jsonplaceholder.typicode.com/posts/1` <br>
+digunakan untuk update data posts <br>
+
+`
+{
+    id: 101,
+    title: 'jackie',
+    body: 'chan',
+    userId: 1,
+}
+`
+
+<br>
+
+4.DELETE `https://jsonplaceholder.typicode.com/posts/1` <br>
+digunakan untuk menghapus data dari API. <br>
